50b6f28d7dc10fdf4e5dbece87ae667b1dffd6ef
songwongbin <bb201802@naver.com>
2024-11-12 09:14:18 +0900

    [add] 3일차 14번, 15번

--- 014.js
+++ 014.js
@@ -1,3 +1,11 @@
 /* 정수 n을 입력받아 n의 약수를 모두 더한 값을 
 리턴하는 함수, solution을 완성해주세요. */
-/* https://school.programmers.co.kr/learn/courses/30/lessons/12928 */
+function solution(n) {
+  var answer = 0;
+  let divisor = 0;
+  while (divisor <= n) {
+    n % divisor === 0 ? (answer += divisor) : answer;
+    divisor++;
+  }
+  return answer;
+}
