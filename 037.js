0f8f84d93e7ae364250c917728e9ae5a91e8d870
songwongbin <bb201802@naver.com>
2024-11-25 10:01:58 +0900

    [add] 036~037번 문제

---
+++ 037.js
@@ -0,0 +1,14 @@
+/* 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 
+2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요. */
+function solution(arr1, arr2) {
+  var answer = [];
+  for (let I = 0; I < arr1.length; I++) {
+    let arr = [];
+    for (let i = 0; i < arr1[I].length; i++) {
+      let sum = arr1[I][i] + arr2[I][i];
+      arr.push(sum);
+    }
+    answer.push(arr);
+  }
+  return answer;
+}
